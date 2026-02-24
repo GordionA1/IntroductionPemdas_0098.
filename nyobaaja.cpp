85286b5a670e32e5fdcb4b82b4371f0165546c5e
GordionA1 <farisazka2506@gmail.com>
2026-02-24 16:15:31 +0700

    first commit

---
+++ nyobaaja.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout<< "reza arap oktavia ganteng gamers idaman"
+    << endl;
+    return 0;
+}
---
+++ perkenalan.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool cek = true;
+    char jk;
+    int angka = 10;
+    float tinggi = 165.5;
+    double harga = 2000000.00;
+    string nama = "paress";
+
+    jk = 'L';
+    cout << "selamat Datang" <<endl;
+    cout << "di TI UMY\n";
+    cout << "Yogyakarta\n";
+
+    nama = "morenn";
+    cout << "nama saya : " << nama;
+}
